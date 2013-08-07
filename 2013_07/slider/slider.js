10f282a32f46154aad4bbb8ce6e678c0ab3198b9
benefit0205 <j.maehashi@leihauoli.com>
2013-08-08 05:07:36 +0900

    slider 追加

---
+++ 2013_07/slider/slider.js
@@ -0,0 +1,54 @@
+$(window).on("load", function () {
+	var crIndex = 1;
+	var $container = $(".container");
+	var $img = $container.find("img");
+	var imgWidth = $img.width();
+	var imgIndex = $img.length;
+	var initPosition = $container.position().left;
+
+	function init() {
+		$container.css("left", initPosition - 800 + "px");
+		console.log(crIndex);
+	}
+
+	function leftSlide() {
+		var initPosition = $container.position().left;
+		if (crIndex == imgIndex - 2) {
+			$container.stop().animate({
+				left: initPosition - imgWidth + "px"
+			}, 500, function () {
+				$(this).css("left", 0);
+			});
+			crIndex = 0;
+		} else {
+			$container.stop().animate({
+				left: initPosition - imgWidth + "px"
+			}, 500);
+			crIndex++;
+		}
+	}
+
+	function rightSlide() {
+		var initPosition = $container.position().left;
+		if (crIndex == 1) {
+			$container.stop().animate({
+				left: initPosition + imgWidth + "px"
+			}, 500, function () {
+				$(this).css("left", -3200 + "px");
+			});
+			crIndex = 4;
+		} else {
+			$container.stop().animate({
+				left: initPosition + imgWidth + "px"
+			}, 500);
+			crIndex--;
+		}
+	}
+
+	init();
+	$("#left").bind('click', leftSlide);
+	$("#right").bind('click', rightSlide);
+
+	//setInterval(leftSlide, 1000);
+});
+
