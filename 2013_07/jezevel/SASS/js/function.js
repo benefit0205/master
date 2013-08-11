ee35c70aa231f347c48b0a4d4eed4c6fb9047e95
benefit0205 <j.maehashi@leihauoli.com>
2013-08-12 08:44:00 +0900

    jezevel 編集

--- 2013_07/jezevel/SASS/js/function.js
+++ 2013_07/jezevel/SASS/js/function.js
@@ -1,5 +1,38 @@
 $(function () {
 
+	//dropDown Menu
+	function setDropNav(){
+		$("#dropNav>li").each(function(){
+			var subMenu = $('>ul', this);
+			var defaultHeight = subMenu.height();
+			$(this).hover(function(){
+				if(subMenu.css('display') == 'none' ){
+					subMenu.slideDown('fast', function(){
+						subMenu.css('overflow', 'visible');
+					});
+				} else {
+					subMenu.stop().animate({
+						height: defaultHeight
+					}, 'fast', function(){
+						subMenu.css('overflow', 'visible');
+					});
+				}
+			}, function(){
+				subMenu.stop().slideUp('fast');
+			});
+		});
+		$("#dropNav ul").prepend('<li class="navBalloon"></li>').hide();
+	}
+
+	function fuga(){
+		$("#dropNav").hide();
+		var $spNavs = $('<ul class="spNavs"></ul>');
+		$spNavs.push('<li class="hoge"></li>');
+		$spNavs.push('<li class="hogehoge"></li>');
+		$("body").append($spNavs);
+	}
+
+	//load&resize event
 	function setBind() {
 		var $container = $(".container");
 		var initWidth = $(window).width();
@@ -13,8 +46,11 @@ $(function () {
 				itemSelector: '.item',
 				isFitWidth: true
 			});
+			setDropNav();
 		}
 	}
+
+
 
 	$(window).bind('load resize', setBind);
 
