9535a611927041e6ab988677b192959474986e70
benefit0205 <j.maehashi@leihauoli.com>
2013-08-12 00:30:27 +0900

    jezebelフォルダ 編集

---
+++ 2013_07/jezevel/SASS/js/function.js
@@ -0,0 +1,21 @@
+$(function () {
+
+	function setBind() {
+		var $container = $(".container");
+		var initWidth = $(window).width();
+		console.log(initWidth);
+
+		if (initWidth <= 540) {
+			$container.masonry('destroy');
+		} else {
+			$container.masonry({
+				columnWidth: 200,
+				itemSelector: '.item',
+				isFitWidth: true
+			});
+		}
+	}
+
+	$(window).bind('load resize', setBind);
+
+});
